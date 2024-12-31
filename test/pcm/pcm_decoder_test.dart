4b70ccd14d671f2bc172302ada57d3b5530297b6
Clement <clement.beal@outlook.com>
2024-12-31 17:19:36 -0300

    add tests for u16_be

--- test/pcm/pcm_decoder_test.dart
+++ test/pcm/pcm_decoder_test.dart
@@ -76,4 +76,99 @@ void main() {
       );
     },
   );
+
+  group(
+    "unsigned 16 bits Big-Endian",
+    () {
+      tearDown(
+        () {
+          // File("a.pcm").deleteSync();
+        },
+      );
+
+      test(
+        "1 channel",
+        () {
+          final a = File("a.pcm");
+          a.writeAsBytesSync([0x0B, 0x2A, 0x23, 0x11]);
+
+          final decoder = PcmDecoder(
+            track: a,
+            sampleRate: 44100,
+            nbChannel: 1,
+            encoding: PCMDecoderEncoding.unsigned16bitsBE,
+          );
+
+          final channels = decoder.decode();
+
+          expect(channels.length, 1);
+          expect(channels[0], [0x0B2A, 0x2311]);
+        },
+      );
+
+      test(
+        "2 channels",
+        () {
+          final a = File("a.pcm");
+          a.writeAsBytesSync([0x0B, 0x2A, 0x23, 0x11]);
+
+          final decoder = PcmDecoder(
+            track: a,
+            sampleRate: 44100,
+            nbChannel: 2,
+            encoding: PCMDecoderEncoding.unsigned16bitsBE,
+          );
+
+          final channels = decoder.decode();
+
+          expect(channels.length, 2);
+          expect(channels[0], [0x0B2A]);
+          expect(channels[1], [0x2311]);
+        },
+      );
+
+      test(
+        "2 channels - lot of data",
+        () {
+          final a = File("a.pcm");
+          a.writeAsBytesSync([0x0B, 0x2A, 0x23, 0x11, 0x01, 0x02, 0x03, 0x04]);
+
+          final decoder = PcmDecoder(
+            track: a,
+            sampleRate: 44100,
+            nbChannel: 2,
+            encoding: PCMDecoderEncoding.unsigned16bitsBE,
+          );
+
+          final channels = decoder.decode();
+
+          expect(channels.length, 2);
+          expect(channels[0], [0x0B2A, 0x0102]);
+          expect(channels[1], [0x2311, 0x0304]);
+        },
+      );
+
+      test(
+        "3 channels",
+        () {
+          final a = File("a.pcm");
+          a.writeAsBytesSync([0x0B, 0x2A, 0x23, 0x11, 0x12, 0x98]);
+
+          final decoder = PcmDecoder(
+            track: a,
+            sampleRate: 44100,
+            nbChannel: 3,
+            encoding: PCMDecoderEncoding.unsigned16bitsBE,
+          );
+
+          final channels = decoder.decode();
+
+          expect(channels.length, 3);
+          expect(channels[0], [0x0B2A]);
+          expect(channels[1], [0x2311]);
+          expect(channels[2], [0x1298]);
+        },
+      );
+    },
+  );
 }
