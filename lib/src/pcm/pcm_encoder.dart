bea562b432dae663635938956df4deb1332ed082
Clement <clement.beal@outlook.com>
2025-01-03 19:02:14 -0300

    add encoder for PCM unsigned little-endian

--- lib/src/pcm/pcm_encoder.dart
+++ lib/src/pcm/pcm_encoder.dart
@@ -37,23 +37,17 @@ class PcmEncoder {
       case PCMEncoding.unsigned8bits:
         return _unsigned8Bits(channels);
       case PCMEncoding.unsigned16bitsBE:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _unsigned16BitsBE(channels);
       case PCMEncoding.unsigned16bitsLE:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _unsigned16BitsLE(channels);
       case PCMEncoding.unsigned24bitsBE:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _unsigned24BitsBE(channels);
       case PCMEncoding.unsigned24bitsLE:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _unsigned24BitsLE(channels);
       case PCMEncoding.unsigned32bitsBE:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _unsigned32BitsBE(channels);
       case PCMEncoding.unsigned32bitsLE:
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        return _unsigned32BitsLE(channels);
     }
   }
 
@@ -96,6 +90,111 @@ class PcmEncoder {
       for (int channel = 0; channel < nbChannel; channel++) {
         buffer.setInt16(
             channel + i * nbChannel * bytesPerSamples, data[channel][i]);
+      }
+    }
+
+    return buffer.buffer.asUint8List();
+  }
+
+  Uint8List _unsigned16BitsBE(List<Samples> data) {
+    int bytesPerSamples = 2;
+
+    final buffer = ByteData(samplesPerChannel * nbChannel * bytesPerSamples);
+
+    for (var i = 0; i < samplesPerChannel; i++) {
+      for (int channel = 0; channel < nbChannel; channel++) {
+        buffer.setUint16(
+            bytesPerSamples * channel + i * nbChannel * bytesPerSamples,
+            data[channel][i]);
+      }
+    }
+
+    return buffer.buffer.asUint8List();
+  }
+
+  Uint8List _unsigned24BitsBE(List<Samples> data) {
+    int bytesPerSamples = 3;
+
+    final buffer = ByteData(samplesPerChannel * nbChannel * bytesPerSamples);
+
+    for (var i = 0; i < samplesPerChannel; i++) {
+      for (int channel = 0; channel < nbChannel; channel++) {
+        int sample = data[channel][i];
+        int offset =
+            bytesPerSamples * channel + i * nbChannel * bytesPerSamples;
+        buffer.setUint8(offset, (sample >> 16) & 0xFF);
+        buffer.setUint8(offset + 1, (sample >> 8) & 0xFF);
+        buffer.setUint8(offset + 2, sample & 0xFF);
+      }
+    }
+
+    return buffer.buffer.asUint8List();
+  }
+
+  Uint8List _unsigned32BitsBE(List<Samples> data) {
+    int bytesPerSamples = 4;
+
+    final buffer = ByteData(samplesPerChannel * nbChannel * bytesPerSamples);
+
+    for (var i = 0; i < samplesPerChannel; i++) {
+      for (int channel = 0; channel < nbChannel; channel++) {
+        buffer.setUint32(
+            bytesPerSamples * channel + i * nbChannel * bytesPerSamples,
+            data[channel][i]);
+      }
+    }
+
+    return buffer.buffer.asUint8List();
+  }
+
+  Uint8List _unsigned16BitsLE(List<Samples> data) {
+    int bytesPerSamples = 2;
+
+    final buffer = ByteData(samplesPerChannel * nbChannel * bytesPerSamples);
+
+    for (var i = 0; i < samplesPerChannel; i++) {
+      for (int channel = 0; channel < nbChannel; channel++) {
+        buffer.setUint16(
+            bytesPerSamples * channel + i * nbChannel * bytesPerSamples,
+            data[channel][i],
+            Endian.little); // Note the Endian.little parameter
+      }
+    }
+
+    return buffer.buffer.asUint8List();
+  }
+
+  Uint8List _unsigned24BitsLE(List<Samples> data) {
+    int bytesPerSamples = 3;
+
+    final buffer = ByteData(samplesPerChannel * nbChannel * bytesPerSamples);
+
+    for (var i = 0; i < samplesPerChannel; i++) {
+      for (int channel = 0; channel < nbChannel; channel++) {
+        int sample = data[channel][i];
+        int offset =
+            bytesPerSamples * channel + i * nbChannel * bytesPerSamples;
+        // Set bytes in little-endian order (LSB first)
+        buffer.setUint8(offset, sample & 0xFF);
+        buffer.setUint8(offset + 1, (sample >> 8) & 0xFF);
+        buffer.setUint8(offset + 2, (sample >> 16) & 0xFF);
+      }
+    }
+
+    return buffer.buffer.asUint8List();
+  }
+
+  Uint8List _unsigned32BitsLE(List<Samples> data) {
+    int bytesPerSamples = 4;
+
+    final buffer = ByteData(samplesPerChannel * nbChannel * bytesPerSamples);
+
+    for (var i = 0; i < samplesPerChannel; i++) {
+      for (int channel = 0; channel < nbChannel; channel++) {
+        buffer.setUint32(
+            bytesPerSamples * channel + i * nbChannel * bytesPerSamples,
+            data[channel][i],
+            Endian.little); // Note the Endian.little parameter
       }
     }
 
--- test/pcm/pcm_encoder_test.dart
+++ test/pcm/pcm_encoder_test.dart
@@ -80,4 +80,254 @@ void main() {
       expect(result, [1, 2, 3, 254, 5, 6, 7, 253, 9, 10, 11, 255]);
     });
   });
+
+  // Unsigned Big-Endian
+
+  group("unsigned 16 bits Big-Endian", () {
+    test("1 channel", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x0102, 0x0304])
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned16bitsBE);
+
+      expect(result, hasLength(4));
+      expect(result, [0x01, 0x02, 0x03, 0x04]);
+    });
+
+    test("2 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x0102]),
+        Int32List.fromList([0x0304]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned16bitsBE);
+
+      expect(result, hasLength(4));
+      expect(result, [0x01, 0x02, 0x03, 0x04]);
+    });
+
+    test("3 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x0104]),
+        Int32List.fromList([0x0205]),
+        Int32List.fromList([0x0306]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned16bitsBE);
+
+      expect(result, hasLength(6));
+      expect(result, [0x01, 0x04, 0x02, 0x05, 0x03, 0x06]);
+    });
+  });
+
+  group("unsigned 24 bits Big-Endian", () {
+    test("1 channel", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x010203, 0x030405])
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned24bitsBE);
+
+      expect(result, hasLength(6));
+      expect(result, [0x01, 0x02, 0x03, 0x03, 0x04, 0x05]);
+    });
+
+    test("2 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x010203]),
+        Int32List.fromList([0x030405]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned24bitsBE);
+
+      expect(result, hasLength(6));
+      expect(result, [0x01, 0x02, 0x03, 0x03, 0x04, 0x05]);
+    });
+
+    test("3 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x010409]),
+        Int32List.fromList([0x020509]),
+        Int32List.fromList([0x030609]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned24bitsBE);
+
+      expect(result, hasLength(9));
+      expect(result, [
+        0x01, 0x04, 0x09, //
+        0x02, 0x05, 0x09, //
+        0x03, 0x06, 0x09,
+      ]);
+    });
+  });
+
+  group("unsigned 32  bits Big-Endian", () {
+    test("1 channel", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x01020304, 0x05060708])
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned32bitsBE);
+
+      expect(result, hasLength(8));
+      expect(result, [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+    });
+
+    test("2 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x01020304]),
+        Int32List.fromList([0x05060708]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned32bitsBE);
+
+      expect(result, hasLength(8));
+      expect(result, [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+    });
+
+    test("3 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x01020304]),
+        Int32List.fromList([0x05060708]),
+        Int32List.fromList([0x090A0B0C]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned32bitsBE);
+
+      expect(result, hasLength(12));
+      expect(result, [
+        0x01, 0x02, 0x03, 0x04, //
+        0x05, 0x06, 0x07, 0x08, //
+        0x09, 0x0A, 0x0B, 0x0C, //
+      ]);
+    });
+  });
+
+  // Unsigned Little-Endian
+
+  // Unsigned Little-Endian
+
+  group("unsigned 16 bits Little-Endian", () {
+    test("1 channel", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x0102, 0x0304])
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned16bitsLE);
+
+      expect(result, hasLength(4));
+      expect(result, [0x02, 0x01, 0x04, 0x03]);
+    });
+
+    test("2 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x0102]),
+        Int32List.fromList([0x0304]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned16bitsLE);
+
+      expect(result, hasLength(4));
+      expect(result, [0x02, 0x01, 0x04, 0x03]);
+    });
+
+    test("3 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x0104]),
+        Int32List.fromList([0x0205]),
+        Int32List.fromList([0x0306]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned16bitsLE);
+
+      expect(result, hasLength(6));
+      expect(result, [0x04, 0x01, 0x05, 0x02, 0x06, 0x03]);
+    });
+  });
+
+  group("unsigned 24 bits Little-Endian", () {
+    test("1 channel", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x010203, 0x030405])
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned24bitsLE);
+
+      expect(result, hasLength(6));
+      expect(result, [0x03, 0x02, 0x01, 0x05, 0x04, 0x03]);
+    });
+
+    test("2 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x010203]),
+        Int32List.fromList([0x030405]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned24bitsLE);
+
+      expect(result, hasLength(6));
+      expect(result, [0x03, 0x02, 0x01, 0x05, 0x04, 0x03]);
+    });
+
+    test("3 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x010409]),
+        Int32List.fromList([0x020509]),
+        Int32List.fromList([0x030609]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned24bitsLE);
+
+      expect(result, hasLength(9));
+      expect(result, [
+        0x09, 0x04, 0x01, //
+        0x09, 0x05, 0x02, //
+        0x09, 0x06, 0x03,
+      ]);
+    });
+  });
+
+  group("unsigned 32 bits Little-Endian", () {
+    test("1 channel", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x01020304, 0x05060708])
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned32bitsLE);
+
+      expect(result, hasLength(8));
+      expect(result, [0x04, 0x03, 0x02, 0x01, 0x08, 0x07, 0x06, 0x05]);
+    });
+
+    test("2 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x01020304]),
+        Int32List.fromList([0x05060708]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned32bitsLE);
+
+      expect(result, hasLength(8));
+      expect(result, [0x04, 0x03, 0x02, 0x01, 0x08, 0x07, 0x06, 0x05]);
+    });
+
+    test("3 channels", () {
+      final encoder = PcmEncoder(channels: [
+        Int32List.fromList([0x01020304]),
+        Int32List.fromList([0x05060708]),
+        Int32List.fromList([0x090A0B0C]),
+      ]);
+
+      final result = encoder.encode(PCMEncoding.unsigned32bitsLE);
+
+      expect(result, hasLength(12));
+      expect(result, [
+        0x04, 0x03, 0x02, 0x01, //
+        0x08, 0x07, 0x06, 0x05, //
+        0x0C, 0x0B, 0x0A, 0x09, //
+      ]);
+    });
+  });
 }
