d93353a7c92fdd6e9082b80a0ea5dc092e82b7b8
clement <clement.beal@outlook.com>
2026-05-11 16:05:44 -0400

    add the empty API

--- lib/src/flac/flac_encoder.dart
+++ lib/src/flac/flac_encoder.dart
@@ -1,1 +1,9 @@
-class FlacEncoder {}
+import 'dart:typed_data';
+
+import 'package:audio_codec/src/flac/flac_decoder.dart';
+
+class FlacEncoder {
+  Uint8List encode(List<Samples> samples) {
+    throw UnimplementedError('FLAC encoding is not implemented yet');
+  }
+}
